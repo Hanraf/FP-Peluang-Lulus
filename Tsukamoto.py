ea62a63784590b099bac101f6ecb1eea8dd45606
Iqmalbasori667 <120441480+Iqmalbasori667@users.noreply.github.com>
2023-06-14 14:37:02 +0700

    Update Tsukamoto.py

--- Tsukamoto.py
+++ Tsukamoto.py
@@ -14,17 +14,17 @@ total_sks = ctrl.Antecedent(np.arange(0, 151, 1), 'Total SKS')
 kemungkinan_lulus = ctrl.Consequent(np.arange(0, 101, 1), 'Kemungkinan Lulus')
 
 # Definisi fungsi keanggotaan untuk variabel input dan output
-nilai_ip['rendah'] = fuzz.trapmf(nilai_ip.universe, [0, 0, 1.5, 2.5])
-nilai_ip['sedang'] = fuzz.trimf(nilai_ip.universe, [1.5, 2.5, 3.5])
-nilai_ip['tinggi'] = fuzz.trapmf(nilai_ip.universe, [2.5, 3.5, 4, 4])
+nilai_ip['kurang'] = fuzz.trapmf(nilai_ip.universe, [0, 0, 0, 2.1])
+nilai_ip['cukup'] = fuzz.trimf(nilai_ip.universe, [2.2, 2.5, 2.7])
+nilai_ip['berprestasi'] = fuzz.trapmf(nilai_ip.universe, [2.8, 3.5, 4, 4])
 
-total_sks['rendah'] = fuzz.trapmf(total_sks.universe, [0, 0, 50, 75])
-total_sks['sedang'] = fuzz.trimf(total_sks.universe, [50, 75, 100])
-total_sks['tinggi'] = fuzz.trapmf(total_sks.universe, [75, 100, 150, 150])
+total_sks['kurang'] = fuzz.trapmf(total_sks.universe, [0, 0, 0, 48])
+total_sks['hampir'] = fuzz.trimf(total_sks.universe, [49, 72, 143])
+total_sks['aman'] = fuzz.trapmf(total_sks.universe, [144, 155, 170, 180])
 
-kemungkinan_lulus['rendah'] = fuzz.trapmf(kemungkinan_lulus.universe, [0, 0, 30, 50])
-kemungkinan_lulus['sedang'] = fuzz.trimf(kemungkinan_lulus.universe, [30, 50, 70])
-kemungkinan_lulus['tinggi'] = fuzz.trapmf(kemungkinan_lulus.universe, [50, 70, 100, 100])
+kemungkinan_lulus['belum lulus'] = fuzz.trapmf(kemungkinan_lulus.universe, [0, 0, 30, 50])
+kemungkinan_lulus['mungkin lulus'] = fuzz.trimf(kemungkinan_lulus.universe, [30, 50, 70])
+kemungkinan_lulus['lulus'] = fuzz.trapmf(kemungkinan_lulus.universe, [50, 70, 100, 100])
 
 # Membuat aturan fuzzy
 rule1 = ctrl.Rule(nilai_ip['rendah'] | total_sks['rendah'], kemungkinan_lulus['rendah'])
